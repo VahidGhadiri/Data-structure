dfe980ae0831c87d1d1d5638a4995bb05650f08e
vahid ghadiri <62473529+VahidGhadiri@users.noreply.github.com>
2020-09-19 10:36:19 +0430

    Add files via upload

---
+++ BinarySearch.js
@@ -0,0 +1,82 @@
+class Node{
+    constructor(value){
+        this.value = value
+        this.right = null
+        this.left = null
+    }
+}
+
+class BST {
+    constructor(value){
+        this.root = new Node(value)
+        this.count = 1
+    }
+
+    size(){
+        return this.count
+    }
+
+    insert(value){
+        this.size++
+        let newNode = new Node(value)
+
+        const searchTree = node => {
+
+            if(value < node.value){
+                if(!node.left){
+                    node.left = newNode
+                }else{
+                    searchTree(node.left)
+                }
+            }
+
+            else if(value > node.value){
+                if(!node.right){
+                    node.right = newNode
+                }else{
+                    searchTree(node.right)
+                }
+            }
+        }
+        searchTree(this.root)
+    }
+
+    min(){
+        let currentNode = this.root
+        
+        while(currentNode.left){
+            currentNode = currentNode.left
+        }
+        return currentNode.value
+    }
+
+    max(){
+        let currentNode = this.root
+
+        while(currentNode.right){
+            currentNode = currentNode.right
+        }
+        return currentNode.value
+    }
+
+    contains(value){
+        let currentNode = this.root
+
+        while(currentNode){
+            if(value === currentNode.value){
+                return true
+            }
+            if(value < currentNode.value){
+                currentNode = currentNode.left
+            }else{
+                if(value > currentNode.value){
+                    currentNode = currentNode.right
+                }
+            }
+        }
+        return false
+    }
+}
+
+
+
